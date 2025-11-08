faadb94dbb07ffd45386a54ba5d1c481177984df
Collin Yoshida <collinyoshida@lcad.edu>
2025-11-08 11:13:48 -0800

    Update Grab Display.cs

--- Assets/Scripts/Grab Display.cs
+++ Assets/Scripts/Grab Display.cs
@@ -4,3 +4,7 @@ public class GrabDisplay : MonoBehaviour
 {
     private void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+
+    }
+}
